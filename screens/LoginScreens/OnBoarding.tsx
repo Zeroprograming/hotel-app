02b24b11c6cdcf43ec643a558ce63bdcce08b5e8
Zero <johanjimenez0210@gmail.com>
2023-06-26 13:22:17 -0500

    [Refactory] Changes for implement new screen

--- screens/LoginScreens/OnBoarding.tsx
+++ screens/LoginScreens/OnBoarding.tsx
@@ -1,13 +1,9 @@
 import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
 import { Pressable, Text, View, TouchableOpacity } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-
-import Navigation from "../../navigation/TabNavigator";
 
 // @ts-ignore
 function OnBoardingScreen({ navigation }) {
-  
   return (
     <View className=" flex-1 items-center justify-center  ">
       <View className="mb-10">
@@ -18,29 +14,46 @@ function OnBoardingScreen({ navigation }) {
           Are you ready to uproot and start over in a new area? Placoo will help
           you on your journey!
         </Text>
-        <TouchableOpacity className="items-center" onPress={() => navigation.navigate("HomeScreen")}>
+        <TouchableOpacity
+          className="items-center"
+          onPress={() => navigation.navigate("LogIn")}
+        >
           <LinearGradient
-            colors={["#FFD600", "#FFA800"]}
+            colors={["#917AFD", "#6246EA"]}
             start={{ x: 0, y: 0 }}
             end={{ x: 1, y: 0 }}
-            style={{
-              height: 50,
-              width: 300,
-              borderRadius: 10,
-              marginTop: 20,
-              justifyContent: "center",
-              alignItems: "center",
-            }}
+            // style={{
+            //   height: 40,
+            //   width: 150,
+            //   borderRadius: 10,
+            //   marginTop: 20,
+            //   justifyContent: "center",
+            //   alignItems: "center",
+            // }}
+            className="items-center justify-center bg-black p-2 rounded-3xl w-[90%] mt-5 h-[50px]"
           >
-            <Text
-              style={{
-                fontSize: 20,
-                fontWeight: "bold",
-                color: "#fff",
-              }}
-            >
-              Get Started
-            </Text>
+            <Text className="text-white text-lg font-semibold">Log In</Text>
+          </LinearGradient>
+        </TouchableOpacity>
+        <TouchableOpacity
+          className="items-center"
+          onPress={() => navigation.navigate("HomeScreen")}
+        >
+          <LinearGradient
+            colors={["#fff", "#fff"]}
+            start={{ x: 0, y: 0 }}
+            end={{ x: 1, y: 0 }}
+            // style={{
+            //   height: 50,
+            //   width: 300,
+            //   borderRadius: 10,
+            //   marginTop: 20,
+            //   justifyContent: "center",
+            //   alignItems: "center",
+            // }}
+            className="items-center justify-center p-2 rounded-3xl w-[90%] mt-3 h-[50px]"
+          >
+            <Text className="text-base font-normal">Sign Up</Text>
           </LinearGradient>
         </TouchableOpacity>
       </View>
